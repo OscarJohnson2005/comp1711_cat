91f4143297296b12df252cc63e807226129e2f84
OscarJohnson2005 <146839647+OscarJohnson2005@users.noreply.github.com>
2023-11-21 14:23:34 +0000

    Work 21/11/23

---
+++ week8/nameArray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main{
+    char name;
+    char nameArray[2][30];
+
+    for(i; i<2; i++)
+    {
+        fgets(nameArray[i], 30, stdin);
+        
+    }
+
+
+
+
+
+
+}
